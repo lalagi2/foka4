29bc60d59cb6a655a3095e371f48f8bb2857b838
lalagi <tothlajosg@gmail.com>
2013-04-23 14:58:12 +0200

    proj

---
+++ foka4/src/foka4.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : foka4.cpp
+// Author      : Toth Lajos
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
